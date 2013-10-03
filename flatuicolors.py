80b8d9cda4888825cb7582b1871d4e256ff8a086
Masato Onodera <masato.onodera@gmail.com>
2013-10-03 13:12:57 +0200

    add functions: pick() and list()

--- flatuicolors.py
+++ flatuicolors.py
@@ -1,24 +1,38 @@
+"""
+A Pytho module to import a hex color string from colors listed Flat UI Colors (http://flatuicolors.com/)
+"""
 
-# Flat UI Colors: http://flatuicolors.com/
+_flatuicolors = {
+    'turquoise':       '#1abc9c',
+    'greensea':        '#16a085',
+    'emerald':         '#2ecc71',
+    'nepharitis':      '#27ae60',
+    'peterriver':      '#3498db',
+    'belizehole':      '#2980b9',
+    'amethyst':        '#9b59b6',
+    'wisteria' :       '#8e44ad',
+    'wetasphalt':      '#34495e',
+    'midnightblue':    '#2c3e50',
+    'sunflower':       '#f1c40f',
+    'orange':          '#f39c12',
+    'carrot':          '#e67e22',
+    'pumpkin':         '#d35400',
+    'alizarin':        '#e74c3c',
+    'pomegranate':     '#c0392b',
+    'clouds':          '#ecf0f1',
+    'silver':          '#bdc3c7',
+    'concrete':        '#95a5a6',
+    'asbestos':        '#7f8c8d'}
 
+def pick(cname):
+    try:
+        return _flatuicolors.get(cname)
+    except:
+        print 'No color %s is in flat ui colors.' % cname
+        print 'Available colors are:'
+        for i in _flatuicolors.keys():
+            print "  %-14s" % i
 
-flatuicolors = {'turquoise': '#1abc9c', 
-                'greensea': '#16a085', 
-                'emerald': '#2ecc71', 
-                'nepharitis': '#27ae60', 
-                'peterriver': '#3498db', 
-                'belizehole': '#2980b9',
-                'amethyst': '#9b59b6', 
-                'wisteria' : '#8e44ad',
-                'wetasphalt': '#34495e', 
-                'midnightblue': '#2c3e50',
-                'sunflower': '#f1c40f', 
-                'orange': '#f39c12',
-                'carrot': '#e67e22', 
-                'pumpkin': '#d35400',
-                'alizarin': '#e74c3c', 
-                'pomegranate': '#c0392b', 
-                'clouds': '#ecf0f1', 
-                'silver': '#bdc3c7', 
-                'concrete': '#95a5a6', 
-                'asbestos': '#7f8c8d'}
+def list():
+    for i in _flatuicolors.keys():
+        print "  %-14s" % i
